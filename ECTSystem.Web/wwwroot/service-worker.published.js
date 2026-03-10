8047cf491b73d6917b9bc980366bd1b2ed5a267d
donellmccoy <donell.mccoy@outlook.com>
2026-03-09 23:04:07 -0400

    Add PWA support with manifest and service worker implementation
    - Created a web manifest file for the ECT System to define app metadata and icons.
    - Implemented a service worker for offline caching and asset management.
    - Added a published service worker script to handle installation, activation, and fetch events.

---
+++ ECTSystem.Web/wwwroot/service-worker.published.js
@@ -0,0 +1,50 @@
+// Caution! Be sure you understand the caveats before publishing an application with
+// temporary caching enabled. See https://aka.ms/blazor-service-worker for details.
+
+// Self-referencing the imported scripts to force the browser to update the service worker
+// when any of the referenced assets change.
+self.importScripts('./service-worker-assets.js');
+self.addEventListener('install', event => event.waitUntil(onInstall(event)));
+self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+
+const cacheNamePrefix = 'offline-cache-';
+const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/];
+const offlineAssetsExclude = [/^service-worker\.js$/];
+
+async function onInstall(event) {
+    console.info('Service worker: Install');
+
+    const assetsRequests = self.assetsManifest.assets
+        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+
+    const cache = await caches.open(cacheName);
+    await cache.addAll(assetsRequests);
+}
+
+async function onActivate(event) {
+    console.info('Service worker: Activate');
+
+    // Delete old caches
+    const cacheKeys = await caches.keys();
+    await Promise.all(cacheKeys
+        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+        .map(key => caches.delete(key)));
+}
+
+async function onFetch(event) {
+    let cachedResponse = null;
+    if (event.request.method === 'GET') {
+        // For all navigation requests, try to serve index.html from cache,
+        // to support offline access without a network.
+        const shouldServeIndexHtml = event.request.mode === 'navigate';
+        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+        const cache = await caches.open(cacheName);
+        cachedResponse = await cache.match(request);
+    }
+
+    return cachedResponse || fetch(event.request);
+}
